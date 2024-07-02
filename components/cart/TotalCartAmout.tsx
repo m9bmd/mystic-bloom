65b277daf8d10b55fa5572117fab968f6a969e07
Miyezo <68035828+m9bmd@users.noreply.github.com>
2024-07-02 14:58:10 +0530

    fixed: totalCartAmount

--- components/cart/TotalCartAmout.tsx
+++ components/cart/TotalCartAmout.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 import { useCartContext } from "./CartContextProvider";
 
@@ -8,10 +8,8 @@ const TotalCartAmount = () => {
 
   useEffect(() => {
     const totalAmount = cart.reduce((sum, cartItem) => {
-      const price =
-        typeof cartItem.price === "string"
-          ? parseFloat(cartItem.price)
-          : cartItem.price;
+      // Ensure price is a number
+      const price = parseFloat(cartItem.price as string) || 0;
       return sum + price * (cartItem.quantity || 1);
     }, 0);
 
@@ -19,10 +17,10 @@ const TotalCartAmount = () => {
   }, [cart]);
 
   return (
-    <div className="flex flex-col  gap-2">
-      <div className="flex flex-col rounded-lg gap-2">
+    <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2 rounded-lg">
         {cart.map((cartItem) => (
-          <div key={cartItem.id} className="flex gap-2 w-full">
+          <div key={cartItem.id} className="flex w-full gap-2">
             <p> {cartItem.product.name}</p>
             <p>x</p>
             <p>{cartItem.quantity}</p>
@@ -30,7 +28,7 @@ const TotalCartAmount = () => {
           </div>
         ))}
       </div>
-      <div className="flex justify-between ">
+      <div className="flex justify-between">
         <p> Total Cart Amount: </p>
         <span className="flex items-center justify-center gap-0.5 font-medium">
           <span className="text-xs">₹</span>
